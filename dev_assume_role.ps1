655b332ba83ad4516c6c01e9d7c5b8e41488d39c
RUDRAPPASHANKAR8585 <150207928+RUDRAPPASHANKAR8585@users.noreply.github.com>
2026-08-10 21:16:36 +0530

    Update AWS credentials and role assumption logic

--- dev_assume_role.ps1
+++ dev_assume_role.ps1
@@ -1,24 +1,33 @@
 # Temporary AWS credentials
-$aws_session_token = "<ACTUAL_SESSION_TOKEN>"
-$aws_secret_access_key = "<ACTUAL_SECRET_ACCESS_KEY>"
-$aws_access_key_id = "<ACTUAL_ACCESS_KEY_ID>"
+
+$aws_session_token = "<SESSION_TOKEN>"
+$aws_secret_access_key = "<SECRET_ACCESS_KEY>"
+$aws_access_key_id = "<ACCESS_KEY_ID>"
 
 # Set initial credentials
+
 $env:AWS_ACCESS_KEY_ID = $aws_access_key_id
 $env:AWS_SECRET_ACCESS_KEY = $aws_secret_access_key
 $env:AWS_SESSION_TOKEN = $aws_session_token
 
 # Role to assume
-$ARN = "arn:aws:iam::<ACCOUNT_ID>:role/<ROLE_NAME>"
-$ROLE = "--role-session-name APPADMVI"
+
+$ARN = "arn:aws:iam::499998932841:role/RIDY_AWS_RDRAR27_APPADMVI"
+$ROLE = "APPADMVI"
 
 # Assume the IAM role
-$OUT = aws sts assume-role --role-arn $ARN $ROLE | ConvertFrom-Json
+
+$OUT = aws sts assume-role `
+    --role-arn $ARN `
+    --role-session-name $ROLE |
+    ConvertFrom-Json
 
 # Extract temporary credentials
+
 $env:AWS_ACCESS_KEY_ID = $OUT.Credentials.AccessKeyId
 $env:AWS_SECRET_ACCESS_KEY = $OUT.Credentials.SecretAccessKey
 $env:AWS_SESSION_TOKEN = $OUT.Credentials.SessionToken
 
 # Verify assumed role
+
 aws sts get-caller-identity
